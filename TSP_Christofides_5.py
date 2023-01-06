6d69d44707ecab784cdac442163de7700f5ec0de
Nathan <nathanburnsds@gmail.com>
2023-01-05 22:58:59 -0500

    Add more to the function doc string

--- TSP_Christofides_5.py
+++ TSP_Christofides_5.py
@@ -14,5 +14,7 @@ def christofides_algorithm(G, start):
     :param start: the first (and last) node in the Hamiltonian cycle.
 
     :return: a weighted Hamiltonian cycle in G that begins and ends at the start input, as well as the sum of the edge weights; represented as a length-2
-        tuple, where the first element is the sum of the edge weights, and the second element is the weighted Hamiltonian cycle, represented similarly as G is.
+        tuple, where the first element is the sum of the edge weights, and the second element is the weighted Hamiltonian cycle, represented similarly as G
+        is (i.e. as a list of length-3 tuples, where the first two elements of a tuple are the two endpoints of an edge in the cycle and the third element
+        is the corresponding edge weight.)
     """
