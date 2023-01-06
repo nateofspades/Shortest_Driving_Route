8884bb968648450a84867301f635f8f5e7dcad68
Nathan <nathanburnsds@gmail.com>
2023-01-05 22:57:19 -0500

    Add more to the function doc string

--- TSP_Christofides_5.py
+++ TSP_Christofides_5.py
@@ -3,11 +3,16 @@
 
 import networkx as nx
 
-def christofides_algorithm(G):
+def christofides_algorithm(G, start):
     """
     This function implements Christofide's algorithm to generate a Hamiltonian cycle for a weighted complete graph satisfying the triangle inequality.
-    :param G: a weighted complete graph satisfying the triangle inequality; represented as a list of length-3 tuples,
-        where the first two elements of a tuple are the two endpoints of an edge and the third element is the corresponding
-        edge weight.
-    :return:
+    It also includes the edge weights in the Hamiltonian cycle, as well as the sum of the edge weights.
+
+    :param G: a weighted complete graph satisfying the triangle inequality; represented as a list of length-3 tuples, where the first two elements of a
+        tuple are the two endpoints of an edge in G and the third element is the corresponding edge weight.
+
+    :param start: the first (and last) node in the Hamiltonian cycle.
+
+    :return: a weighted Hamiltonian cycle in G that begins and ends at the start input, as well as the sum of the edge weights; represented as a length-2
+        tuple, where the first element is the sum of the edge weights, and the second element is the weighted Hamiltonian cycle, represented similarly as G is.
     """
