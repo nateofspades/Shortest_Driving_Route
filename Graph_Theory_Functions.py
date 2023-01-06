36007a742a457fe63f2c2c6a2a493ff4c0e8d535
Nathan <nathanburnsds@gmail.com>
2023-01-05 23:44:52 -0500

    Making progress

--- Graph_Theory_Functions.py
+++ Graph_Theory_Functions.py
@@ -1,17 +1,31 @@
 import networkx as nx
 
-# 1. Calculate minimum spanning tree T of G.
-G = nx.cycle_graph(4)
-G.add_edge(0, 3, weight=2)         # See H in step 5 for a more convenient way to create a graph that skips this step.
-T = nx.minimum_spanning_tree(G)
-print(G)
-print(list(G))
-print(G.edges(data=True))
-print(G.edges[0,3]['weight'])
-print(G.edges[0,3])
-print(T.nodes, T.edges, '\n')
 
-# 2. Calculate the set of vertices O with odd degree in T
+
+# Step 1: Convert G to the appropriate format so that it can be used by the networkx library.
+G = [("A", "B", 7), ("A", "C", 2), ("B", "C", 3), ("B", "D", 1), ("C", "E", 4), ("D", "E", 6)]
+print(G)
+G_temp = []
+for edge in G:
+    G_temp.append((edge[0], edge[1], {'weight': edge[2]}))
+G = nx.Graph(G_temp)
+T = nx.minimum_spanning_tree(G)
+print(T.edges(data=True))
+print(G.edges(data=True))
+
+
+
+
+# 2. Calculate minimum spanning tree T of G.
+# G = nx.cycle_graph(4)
+# G.add_edge(0, 3, weight=2)         # See H in step 5 for a more convenient way to create a graph that skips this step.
+# T = nx.minimum_spanning_tree(G)
+# print(G.edges(data=True))
+# print(T.nodes, T.edges, '\n')
+
+
+
+# 3. Calculate the set of vertices O with odd degree in T
 # G = nx.Graph()
 # E2  = [("A", "B"), ("A", "C"), ("B", "C"), ("B", "D"), ("C", "E"), ("D", "E")]
 # G.add_edges_from(E2)
@@ -19,7 +33,7 @@ print(T.nodes, T.edges, '\n')
 # print(G.edges)
 # print(list(G.degree(['A', 'B', 'C', 'D', 'E'])))
 
-# 3. Form the subgraph of G using only the vertices of O
+# 4. Form the subgraph of G using only the vertices of O
 # G = nx.Graph()
 # E2  = [("A", "B"), ("A", "C"), ("B", "C"), ("B", "D"), ("C", "E"), ("D", "E")]
 # G.add_edges_from(E2)
@@ -28,40 +42,41 @@ print(T.nodes, T.edges, '\n')
 # print(H.nodes)
 
 
-# 4. Construct a minimum-weight perfect matching M in this subgraph
+# 5. Construct a minimum-weight perfect matching M in this subgraph
 ### (In our case a minimum-weight maximal matching)
 # G = nx.Graph()
 # edges = [("A", "B", 6), ("A", "C", 2), ("B", "C", 1), ("B", "D", 7), ("C", "E", 9), ("D", "E", 3)]
 # G.add_weighted_edges_from(edges)
 # print(sorted(nx.min_weight_matching(G)), '\n')  # Prints [('C', 'B'), ('D', 'E')]
 
-# 5. Unite matching and spanning tree T ∪ M to form an Eulerian multigraph H; unions the edge sets and vertex sets
+# 6. Unite matching and spanning tree T ∪ M to form an Eulerian multigraph H; unions the edge sets and vertex sets
 # G = nx.Graph([("A", "B"), ("A", "C"), ("B", "C"), ("B", "D"), ("C", "E"), ("D", "E")])
 # H = nx.Graph([('A', 'E'), ('F', 'G')])
 # R = nx.compose(G, H)
 # print(R.nodes)
 # print(R.edges)
 
-# 6. Calculate Euler tour in H
+# 7. Calculate Euler tour in H
 # G = nx.Graph([("E", "D"), ("C", "B"), ("A", "B"), ("D", "A"), ("A", "C"), ("E", "A")])
 # print(list(nx.eulerian_circuit(G, source="A")))  # source is the starting node
 
-# 7. Remove repeated vertices, giving the algorithm's output.
+# 8. Remove repeated vertices, giving the algorithm's output.
 #def remove_repeated_vertices(euler_tour):
 
-euler_tour = [('A', 'B'), ('B', 'C'), ('C', 'A'), ('A', 'D'), ('D', 'E'), ('E', 'A')]
 
-hamiltonian_cycle = []
-hamiltonian_cycle.append(euler_tour[0][0])
-hamiltonian_cycle.append(euler_tour[0][1])
-for edge in euler_tour[1:]:
-    if edge[1] not in hamiltonian_cycle:
-        hamiltonian_cycle.append(edge[1])
-
-# The hamiltonian cycle should end with the same vertex it started with
-hamiltonian_cycle.append(euler_tour[0][0])
-
-print(hamiltonian_cycle)
+# euler_tour = [('A', 'B'), ('B', 'C'), ('C', 'A'), ('A', 'D'), ('D', 'E'), ('E', 'A')]
+#
+# hamiltonian_cycle = []
+# hamiltonian_cycle.append(euler_tour[0][0])
+# hamiltonian_cycle.append(euler_tour[0][1])
+# for edge in euler_tour[1:]:
+#     if edge[1] not in hamiltonian_cycle:
+#         hamiltonian_cycle.append(edge[1])
+#
+# # The hamiltonian cycle should end with the same vertex it started with
+# hamiltonian_cycle.append(euler_tour[0][0])
+#
+# print(hamiltonian_cycle)
 
 
 
--- TSP_Christofides_5.py
+++ TSP_Christofides_5.py
@@ -11,10 +11,34 @@ def christofides_algorithm(G, start):
     :param G: a weighted complete graph satisfying the triangle inequality; represented as a list of length-3 tuples, where the first two elements of a
         tuple are the two endpoints of an edge in G and the third element is the corresponding edge weight.
 
-    :param start: the first (and last) node in the Hamiltonian cycle.
+    :param start: the first (and last) node in the Hamiltonian cycle; represented as a string.
 
     :return: a weighted Hamiltonian cycle in G that begins and ends at the start input, as well as the sum of the edge weights; represented as a length-2
         tuple, where the first element is the sum of the edge weights, and the second element is the weighted Hamiltonian cycle, represented similarly as G
         is (i.e. as a list of length-3 tuples, where the first two elements of a tuple are the two endpoints of an edge in the cycle and the third element
-        is the corresponding edge weight.)
+        is the corresponding edge weight).
     """
+
+    # Step 1: Convert G to the appropriate format so that it can be used by the networkx library.
+    G_temp = []
+    for edge in G:
+        G_temp.append((edge[0], edge[1], {'weight': edge[2]}))
+    G = nx.Graph(G_temp)
+
+    # Step 2: Calculate minimum spanning tree T of G.
+    T = nx.minimum_spanning_tree(G)
+
+    # Step 3: Calculate the set of vertices O with odd degree in T.
+
+
+    # Step 4: Form the subgraph of G using only the vertices of O.
+
+    # Step 5: Construct a minimum-weight perfect matching M in this subgraph
+
+    # Step 6: Unite matching and spanning tree T ∪ M to form an Eulerian multigraph H; unions the edge sets and vertex sets
+
+    # Step 7: Calculate Euler tour in H
+
+    # Step 8: Remove repeated vertices
+
+    # Step 9: (NOT FROM WIKI PAGE: NEED TO INCLUDE WEIGHTS BACK IN PLUS ADD TOTAL WEIGHT)
