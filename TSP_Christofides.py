b2c2ac0339b9b5759847c14323bd0d461c3f9ccb
Nathan <nathanburnsds@gmail.com>
2023-01-07 23:23:46 -0500

    Made some new iterations of files; soon to be finalized

--- TSP_Christofides.py
+++ TSP_Christofides.py
@@ -3,7 +3,7 @@
 
 import networkx as nx
 
-def christofides_algorithm(G, start):
+def tsp_christofides(G, start):
     """
     This function implements Christofide's algorithm to generate a Hamiltonian cycle for a weighted complete graph satisfying the triangle inequality.
     It also includes the edge weights in the Hamiltonian cycle, as well as the sum of the edge weights.
@@ -20,10 +20,7 @@ def christofides_algorithm(G, start):
     """
 
     # Step 1: Convert G to the appropriate format so that it can be used by the networkx library.
-    G_temp = []
-    for edge in G:
-        G_temp.append((edge[0], edge[1], {'weight': edge[2]}))
-    G = nx.Graph(G_temp)
+    G = convert_graph_to_networkx_form(G)
 
     # Step 2: Calculate a minimum spanning tree T of G.
     T = nx.minimum_spanning_tree(G)
@@ -71,3 +68,49 @@ def christofides_algorithm(G, start):
         total_edge_weight += weight
 
     return total_edge_weight, hamiltonian_cycle_weighted_edge_form
+
+def convert_graph_to_networkx_form(G):
+    """
+    This is a helper function for tsp_christofides(). It converts the input graph G into a format that allows it to be input into various networkx functions.
+    :param G: A dictionary in which the keys are the nodes of a weighted graph,
+              and the value for a node N is a dictionary of all nodes as keys and their edge weights with N as values.
+    :return: A networkx graph object.
+    """
+
+    # Convert G to a list of length-3 tuples, where the first two elements of a tuple are nodes of G and the third is the weight of the edge joining these nodes.
+    G_list_form = []
+    for key1, value1 in G.items():
+        for key2, value2 in value1.items():
+            if key1 < key2:
+                G_list_form.append((key1, key2, value2))
+
+    # Convert G to a networkx graph object.
+    G_networkx_form = []
+    for edge in G_list_form:
+        G_networkx_form.append((edge[0], edge[1], {'weight': edge[2]}))
+    G_networkx_form = nx.Graph(G_networkx_form)
+
+    return G_networkx_form
+
+
+# Test case 1: 4 nodes
+G = {
+  'A': {'A': 0, 'B': 9, 'C': 6, 'D': 8},
+  'B': {'A': 9, 'B': 0, 'C': 3, 'D': 2},
+  'C': {'A': 6, 'B': 3, 'C': 0, 'D': 5},
+  'D': {'A': 8, 'B': 2, 'C': 5, 'D': 0}
+}
+start = 'A'
+print(tsp_christofides(G, 'A'))  # Prints ['A', 'C', 'B', 'D', 'A']
+
+
+# Test case 2: 5 nodes
+G = {
+  'A': {'A': 0, 'B': 10, 'C': 20, 'D': 30, 'E': 40},
+  'B': {'A': 10, 'B': 0, 'C': 15, 'D': 25, 'E': 35},
+  'C': {'A': 20, 'B': 15, 'C': 0, 'D': 5, 'E': 15},
+  'D': {'A': 30, 'B': 25, 'C': 5, 'D': 0, 'E': 10},
+  'E': {'A': 40, 'B': 35, 'C': 15, 'D': 10, 'E': 0}
+}
+start = 'A'
+print(tsp_christofides(G, 'A'))  # Prints ['A', 'B', 'C', 'D', 'E', 'A']
