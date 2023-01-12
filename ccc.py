ba5d59c91d78670930b75e177d843f5b616f8bcf
Nathan <nathanburnsds@gmail.com>
2023-01-12 18:40:40 -0500

    Adding gas station functions

--- ccc.py
+++ ccc.py
@@ -1,27 +1,56 @@
 import pprint
 
+nearest_gas_stations = {
+    'A': ['gas_A', 2],
+    'B': ['gas_B', 4],
+    'C': ['gas_C', 3],
+    'D': ['gas_D', 6],
+    'E': ['gas_E', 8]
+}
+print(nearest_gas_stations)
+
 G = {
-    'A': {'A': 0, 'B': 12, 'C': 3, 'D': 6, 'E': 8},
-    'B': {'A': 12, 'B': 0, 'C': 4, 'D': 4, 'E': 5},
-    'C': {'A': 3, 'B': 4, 'C': 0, 'D': 9, 'E': 1},
-    'D': {'A': 6, 'B': 4, 'C': 9, 'D': 0, 'E': 4},
-    'E': {'A': 8, 'B': 5, 'C': 1, 'D': 4, 'E': 0}
+    'A': {'A': 0, 'B': 120, 'C': 30, 'D': 60, 'E': 80},
+    'B': {'A': 120, 'B': 0, 'C': 4, 'D': 4, 'E': 50},
+    'C': {'A': 30, 'B': 40, 'C': 0, 'D': 90, 'E': 10},
+    'D': {'A': 60, 'B': 40, 'C': 90, 'D': 0, 'E': 40},
+    'E': {'A': 80, 'B': 50, 'C': 10, 'D': 40, 'E': 0}
 }
 unvisited_nodes = ['B', 'C', 'E']
-new_dict = {node_outer: {node_inner: G[node_outer][node_inner] for node_inner in unvisited_nodes} for node_outer in unvisited_nodes}
-pprint.pprint(new_dict)
+# new_dict = {node_outer: {node_inner: G[node_outer][node_inner] for node_inner in unvisited_nodes} for node_outer in unvisited_nodes}
+# pprint.pprint(new_dict)
 
-# def update_G(G, unvisited_nodes, current_node):
-#     if current_node in unvisited_nodes:
-#         updated_G = {node_outer: {node_inner: G[node_outer][node_inner] for node_inner in unvisited_nodes} for node_outer in unvisited_nodes}
+# def update_G(G, unvisited_nodes, gas_station):
 #
+#     updated_G = {node_outer: {node_inner: G[node_outer][node_inner] for node_inner in unvisited_nodes} for node_outer in unvisited_nodes}
+#     for node in updated_G
 #     else:
 #         updated_G = {node_outer: {node_inner: G[node_outer][node_inner] for node_inner in unvisited_nodes} for node_outer in unvisited_nodes}
 #
 #
 #     return updated_G
 
-# pprint.pprint(update_G(G, unvisited_nodes, 'gas station'))
+### Already been to 'A' and D'; updating G at 'gas_D'
+# Compute distance from current 'gas_station' to remaining nodes B, C and E   - will need a function for this
+gas_station = {
+    'gas_station': 0,
+    'B': 2,
+    'C': 4,
+    'E': 1
+}
 
-if current_location_type != 'gas station':
-    #
+updated_G = {node_outer: {node_inner: G[node_outer][node_inner] for node_inner in unvisited_nodes} for node_outer in unvisited_nodes}
+for node in updated_G:
+    updated_G[node]['gas_station'] = gas_station[node]
+
+updated_G['gas_station'] = gas_station
+
+
+pprint.pprint(updated_G)
+
+# for node in updated_G:
+#     ngs = nearest_gas_stations[node]
+#     edges = updated_G[node]
+#     edges[neares]
+#     node[nearest_gas_stations[]]
+
