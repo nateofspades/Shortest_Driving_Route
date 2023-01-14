cebc61ab93783640da5875cb8427d530c9d5b3e5
Nathan <nathanburnsds@gmail.com>
2023-01-14 14:32:19 -0500

    Creating functions for mapbox

--- ddd.py
+++ ddd.py
@@ -1,1 +1,13 @@
-https://api.mapbox.com/directions/v5/mapbox/driving/-122.42,37.78;-77.03,38.91?access_token=pk.eyJ1IjoibmF0aGFuYnVybnNkcyIsImEiOiJjbGN1cmx1enQwMHRtM3NxazJzNWY2b2g3In0.4uZgJ-T7QB9--fbCfli92Q
+# Input is a list of coordinate pairs, e.g. [(37.77584, -95.44304), (37.77714, -95.44361), (37.7878, -95.43828)]
+# We want to convert this list to a dictionary
+
+# def create_G(coordinate_list):
+
+
+pairs = ['0','1','2']
+
+distances{
+    '0':
+}
+
+
