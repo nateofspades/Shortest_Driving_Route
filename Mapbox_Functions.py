c5670137e5bfc748b23fe207dee2e63f2c7d1c9f
Nathan <nathanburnsds@gmail.com>
2023-01-17 04:07:34 -0500

    Adding comments for Mapbox_Functions

--- Mapbox_Functions.py
+++ Mapbox_Functions.py
@@ -102,7 +102,7 @@ def generate_route(G, start, is_asymmetric=True):
     at the same location that it started.
     :param G: A graph for which we want to compute a Hamiltonian cycle or path.
     :param start: The starting node (location) of the truck's journey.
-    :param is_asymmetric: If set to true then the truck will not return to its starting node (location) once all of the
+    :param is_asymmetric: If set to True then the truck will not return to its starting node (location) once all of the
         nodes (locations) have been visited. If set to False the truck will return to its starting node (location) once
         all of the nodes (locations) have been visited, so the last node and start node will be equal.
     :return: A Hamiltonian cycle or path of G.
@@ -181,21 +181,21 @@ def waypoint_list_full_route(full_route, access_token):
 
 def cumulative_distance_along_waypoints(waypoint_list_full_route, access_token):
     """
-    A list with one entry for each waypoint in the full_route, where entry i is the cumulative distance (in meters) traveled
-    in the full route up to that waypoint. This is used to predict when to warn the truck driver that he or she will need
-    to fill up on gas.
+    This is used to predict when to warn the truck driver that he or she will need to fill up on gas.
     :param waypoint_list_full_route: A list of waypoints along the full Hamiltonian cycle or path. It is of the form
         [(latitude_1, longitude_1), (latitude_2, longitude_2), ...].
     :param access_token:
-    :return:
+    :return: A list with one entry for each waypoint in the full route, where entry i is the cumulative distance (in meters) traveled
+    in the full route up to that waypoint.
     """
 
-
-
     n = len(waypoint_list_full_route)
+
+    # Initialize the cumulative distance at 0, and also the list which will store the cumulative_distances.
     cumulative_distance = 0
     cumulative_distance_list = [0]
 
+    # Calculate the cumulative distance of the route at each waypoint and store it in the list.
     for i in range(1, n):
         # To save API calls perhaps replace distance_between_nodes with haversine.
         cumulative_distance += haversine(waypoint_list_full_route[i - 1], waypoint_list_full_route[i])
@@ -204,13 +204,26 @@ def cumulative_distance_along_waypoints(waypoint_list_full_route, access_token):
     return cumulative_distance_list
 
 
-
 def predict_waypoints_where_to_search_for_gas(cumulative_distance_along_waypoints, waypoint_list_full_route, start_tank_kms, full_tank_kms, min_tank_tolerance_kms=20):
     """
-    This function assumes that each time the truck stops at a gas station it fills its tank completely.
-    Moreover, it assumes that the truck had to go off the main route to reach the gas station, and that as a consequence it
-    spends 5km leaving the main route and 5km returning to the main route before continuing its journey to the next node.
+    This function predicts when the truck will need to begin to look for a gas station. This function assumes that each
+    time the truck stops at a gas station the tank is filled completely. Moreover, it assumes that the truck has to go off
+    the main route to reach the gas station, and that as a consequence it spends 5km leaving the main route and 5km returning
+    to the main route before continuing the journey to the next node.
+    :param cumulative_distance_along_waypoints: A list with one entry for each waypoint in the full route, where entry i
+        is the cumulative distance (in meters) traveled in the full route up to that waypoint.
+    :param waypoint_list_full_route: A list of waypoints along the full Hamiltonian cycle or path. It is of the form
+        [(latitude_1, longitude_1), (latitude_2, longitude_2), ...].
+    :param start_tank_kms: How many kilometers worth of gas the truck driver has in the tank at the beginning of the route.
+    :param full_tank_kms: The maximum distance (in kilometers) the truck can be driven when the gas tank is full.
+    :param min_tank_tolerance_kms: The gas driver is planning their route such that they never have less than this many
+        kilometers worth of gas in their tank. Typically it is >0 because it would be risky always arriving at gas
+        stations with just enough gas to make it there.
+    :return: A list of waypoints representing predicted locations where the truck driver should start looking for gas.
+        It is of the form [(latitude_1, longitude_1), (latitude_2, longitude_2), ...].
     """
+
+    # Convert cumulative_distance_along_waypoints to a NumPy array so that broadcasting can be used.
     A = np.array(cumulative_distance_along_waypoints)
     n = len(A)
 
@@ -221,6 +234,9 @@ def predict_waypoints_where_to_search_for_gas(cumulative_distance_along_waypoint
     index = 0
     index_list = []
 
+    # Search for the furthest waypoint the truck can make it to from the starting node until it must fill up on gas.
+    # When that waypoint is reached, subtract the distance traveled from the cumulative distance for all of the
+    # remaining waypoints.
     for i in range(1, n):
         if A[i] <= max_initial_distance_before_search_gas:
             continue
@@ -228,9 +244,15 @@ def predict_waypoints_where_to_search_for_gas(cumulative_distance_along_waypoint
             index = i - 1
             index_list.append(index)
             A -= A[i - 1]
-            #print(A, index_list, '\n')
             break
 
+    # Search for the furthest waypoint the truck can make it from its starting node until it must fill up on gas.
+    # When that waypoint is reached, subtract the distance traveled from the cumulative distance for all of the
+    # remaining waypoints.
+
+    # From the last waypoint where the gas tanked was filled up, search for the furthest waypoint the truck can make it to
+    # until it must fill up on gas again. When that waypoint is reached, subtract the distance traveled since the last
+    # gas waypoint from the cumulative distance for all of the remaining waypoints. Repeat until the end of the journey.
     for i in range(index, n):
         if A[i] <= max_distance_before_searching_gas_with_full_tank:
             continue
@@ -238,9 +260,9 @@ def predict_waypoints_where_to_search_for_gas(cumulative_distance_along_waypoint
             index = i - 1
             index_list.append(index)
             A -= A[i - 1]
-            #print(A, index_list, '\n')
             continue
 
+    # Extract the waypoints where it is predicted that a gas tank fill up is needed.
     waypoints = list(np.array(waypoint_list_full_route)[index_list])
 
     return waypoints
@@ -248,8 +270,24 @@ def predict_waypoints_where_to_search_for_gas(cumulative_distance_along_waypoint
 
 def generate_map(access_token, node_list, start, start_tank_kms, full_tank_kms, min_tank_tolerance_kms=20, is_asymmetric=True):
     """
-    Putting it all together.
+    This function generates an interactive map of the Hamiltonian cycle or path.
+    :param access_token: A Mapbox API access token.
+    :param node_list: A list corresponding to all of the nodes that will be visited in the route. It is of the form
+        [(latitude_1, longitude_1), (latitude_2, longitude_2), ...]. Note that the nodes do not need to be ordered
+        according to the order that they will be visited in the route; the generate_route() function computes the ordering.
+    :param start: The starting node (location) of the truck's journey.
+    :param start_tank_kms: How many kilometers worth of gas the truck driver has in the tank at the beginning of the route.
+    :param full_tank_kms: The maximum distance (in kilometers) the truck can be driven when the gas tank is full.
+    :param min_tank_tolerance_kms: The gas driver is planning their route such that they never have less than this many
+        kilometers worth of gas in their tank. Typically it is >0 because it would be risky always arriving at gas
+        stations with just enough gas to make it there.
+    :param is_asymmetric: If set to True then the truck will not return to its starting node (location) once all of the
+        nodes (locations) have been visited. If set to False the truck will return to its starting node (location) once
+        all of the nodes (locations) have been visited, so the last node and start node will be equal.
+    :return: An interactive map of the Hamiltonian cycle or path.
     """
+
+    # Call the functions that are used to generate the interactive map.
     G = create_G(node_list, access_token)
     full_route = generate_route(G, start, is_asymmetric)
     ordered_nodes = ordered_node_list(full_route)
@@ -260,12 +298,13 @@ def generate_map(access_token, node_list, start, start_tank_kms, full_tank_kms, 
     # Extract the first point of the route
     first_point = waypoint_list_full[0]
 
-    # Create a new map
+    # Create a new map.
     m = folium.Map(location=[first_point[1], first_point[0]], zoom_start=13)
 
-    # Add the route to the map
+    # Add the route to the map.
     folium.PolyLine(waypoint_list_full, color="purple", weight=2.5, opacity=1).add_to(m)
 
+    # For the second to second-last nodes, create a marker on the map and label it 'delivery point'.
     for i in range(1, len(ordered_nodes) - 1):
         folium.Marker(
             location=[ordered_nodes[i][0], ordered_nodes[i][1]],
@@ -273,6 +312,8 @@ def generate_map(access_token, node_list, start, start_tank_kms, full_tank_kms, 
             icon=folium.Icon(color='blue', icon='marker')
         ).add_to(m)
 
+    # For the asymmetric case, create a marker on the map for the first node labeled 'start of route', and create a
+    # marker on the map for the last node labeled 'last delivery point and end of route'.
     if is_asymmetric:
 
         folium.Marker(
@@ -287,6 +328,7 @@ def generate_map(access_token, node_list, start, start_tank_kms, full_tank_kms, 
             icon=folium.Icon(color='blue', icon='marker')
         ).add_to(m)
 
+    # For the symmetric case, create a marker on the map for the first/last node labeled 'start and end of route'.
     if not is_asymmetric:
         folium.Marker(
             location=[ordered_nodes[0][0], ordered_nodes[0][1]],
@@ -294,6 +336,7 @@ def generate_map(access_token, node_list, start, start_tank_kms, full_tank_kms, 
             icon=folium.Icon(color='blue', icon='marker')
         ).add_to(m)
 
+    # For each predicted gas station, create a marker on the map labeled 'Look for gas station!'.
     for waypoint in waypoints_where_to_search_for_gas:
         folium.Marker(
             location=[waypoint[0], waypoint[1]],
